8b85d4b8045da2bf9e8dbe361521220a5b9f1fcc
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-24 20:13:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
 def square_array(numbers)
   new_numbers = []
-  ## numbers.each do |number|
-  ##  new_numbers << number ** 2
- ## end
- ## new_numbers
- numbers.collect |number|
-  number ** 2
+  numbers.each do |number|
+    new_numbers << number ** 2
+  end
+  new_numbers
 end
-
